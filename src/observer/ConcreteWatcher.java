68733165e4ecc2e1e30c02839ad905542cb5a090
Li Zheng <lixiaozheng.good@163.com>
2016-06-03 12:24:19 +0800

    观察者模式

---
+++ src/observer/ConcreteWatcher.java
@@ -0,0 +1,12 @@
+package observer;
+
+/**
+ * Created by 李争 on 2016/6/3 0003.
+ * 
+ */
+public class ConcreteWatcher implements Watcher {
+    @Override
+    public void update(String str) {
+        System.out.println("ConcreteWatcher.update"+str);
+    }
+}
